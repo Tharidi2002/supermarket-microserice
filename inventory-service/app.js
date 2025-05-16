8ad1416fa0e84d559d15272c8554f6458de61aa7
Tarindhi <tharidithisarani7@gmail.com>
2025-05-16 21:45:11 +0530

    final commit

--- inventory-service/app.js
+++ inventory-service/app.js
@@ -1,6 +1,7 @@
 // import express from "express"        // new
 
 const express = require('express')     //old
+const {Eureka} = require('eureka-js-client')    // const Eureka = require('eureka-js-client').Eureka;
 
 const port = 3000;
 const app = express()
@@ -17,7 +18,39 @@ router.get('/api/v1/inventory', (req, res) => {
 
 app.use("/inventory-service", router)
 
+const client = new Eureka({
+    // application instance information
+    instance: {
+        app: 'INVENTORY-SERVICE',
+        hostName: 'localhost',
+        ipAddr: '127.0.0.1',
+        port: {
+            $: port,
+            "@enabled": "true"
+        },
+        vipAddress: 'inventory-service',
+        dataCenterInfo: {
+            "@class": "com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo",
+            name: 'MyOwn',
+        },
+    },
+    eureka: {
+        // eureka server host / port
+        host: 'localhost',
+        port: 8761,
+        servicePath: "/eureka/apps/"
+    },
+});
+
 app.listen(port, () => {
     console.log("Server is running on port " + port)
     console.log(`at port: ${port}`)
+
+    client.start((error) => {
+        if (error) {
+            console.log('Eureka client failed to start!', error);
+        }else {
+            console.log('Eureka client started successfully!');
+        }
+    });
 })
